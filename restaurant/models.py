c0cb9256c0380fd5bbad2fdcd71411a2b9a2a06c
Denys Seliverstov <catsperr322@gmail.com>
2023-09-22 01:50:19 +0300

    Adding validation for menu and str repr for all models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 
 class DishType(models.Model):
     name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Dish(models.Model):
@@ -12,9 +16,15 @@ class Dish(models.Model):
                                   related_name="dishes")
     description = models.TextField(null=True, blank=True)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Menu(models.Model):
@@ -24,3 +34,34 @@ class Menu(models.Model):
                                    related_name="menus")
     day = models.DateField(auto_now_add=True)
     dishes = models.ManyToManyField(Dish, related_name="menus")
+
+    @classmethod
+    def validate_day(cls, day, error_to_raise) -> None:
+        if cls.objects.filter(day=day).exists():
+            raise error_to_raise(
+                {
+                    "day":
+                        "You`ve already upload menu today! Try again tomorrow"
+                }
+            )
+
+    def clean(self) -> None:
+        Menu.validate_day(self.day, ValidationError)
+
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ) -> None:
+        self.full_clean()
+        return super(Menu, self).save(
+            force_insert,
+            force_update,
+            using,
+            update_fields
+        )
+
+    def __str__(self) -> str:
+        return self.title
