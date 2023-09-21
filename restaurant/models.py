fb19c40207aa684cad195f9774e4b11a37e22b63
Denys Seliverstov <catsperr322@gmail.com>
2023-09-21 18:33:50 +0300

    Initialising restaurant app with its models

---
+++ restaurant/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    restaurant = models.ForeignKey(Restaurant,
+                                   on_delete=models.CASCADE,
+                                   related_name="menus")
+    day = models.DateField(auto_now_add=True)
+
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    dish_type = models.ForeignKey(DishType,
+                                  on_delete=models.CASCADE,
+                                  related_name="dishes")
+    description = models.TextField(null=True, blank=True)
